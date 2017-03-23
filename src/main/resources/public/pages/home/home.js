60c27a9735b0c80230915454591b6cc5364fe8c2
XiRuiQiang <XiRuiQiang@hotmail.com>
2017-03-23 19:42:40 +0800

    分页

--- src/main/resources/public/pages/home/home.js
+++ src/main/resources/public/pages/home/home.js
@@ -21,21 +21,8 @@ bdtApp.controller('homeController', function($scope, UserService, HttpService,
 	}
 	$scope.getRoles();
 
-	$scope.placement = {
-		options : [ 5, 10, 20, 50 ],
-		pageSize : 5,
-		selected : 20,
-		changeFn : function(res) {
-			// 执行的函数
-			console.log($scope.placement.selected);
-		}
-	};
-
 	// 翻页
 	$scope.totalItems = 1000; // 所有页面中的项目总数
 	$scope.currentPage = 4; // 当前页
-	$scope.setPage = function(pageNo) {
-		$scope.currentPage = pageNo;
-	};
-	$scope.maxSize = 5;
+	$scope.maxSize = 8;
 });
